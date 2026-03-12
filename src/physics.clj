110b8f7520c0f24387d631b2f569dd6107888ea4
Gustavo Lins <gustavolinssm@gmail.com>
2026-03-12 20:13:24 -0300

    feat: add validation to ensure arguments are numbers

--- src/physics.clj
+++ src/physics.clj
@@ -1,16 +1,23 @@
 (ns physics)
 
+(defn verify-nums [xs]
+  (when (some nil? xs)
+    (throw (ex-info "Argument is missing." {:args xs})))
+  (when-not (every? number? xs)
+    (throw (ex-info "Arguments must be numbers." {:args xs}))))
+
 ; Δ = final - inicial
-(defn delta [initial final]
+(defn delta [initial final] 
   (- final initial))
 
 ; f'(x) ≈ Δf / Δt
-(defn rate [a0 a1 b0 b1]
+(defn rate [a0 a1 b0 b1] 
+  (verify-nums [a0 a1 b0 b1]) 
   (/ (delta a0 a1)
      (delta b0 b1)))
 
 ; Vm = Δs / Δt
-(defn velocity [s0 s1 t0 t1]
+(defn average-velocity [s0 s1 t0 t1]
   (rate s0 s1 t0 t1))
 
 ; a = Δv / Δt
@@ -19,4 +26,5 @@
 
 ; S = S0 + v*t
 (defn position [s0 v t]
+  (verify-nums [s0 v t]) 
   (+ s0 (* v t)))
--- test/physics_test.clj
+++ test/physics_test.clj
@@ -10,26 +10,38 @@
   (is (= 2 (rate 0 10 0 5)))
   (is (= 5 (rate 0 20 0 4))))
 
-(deftest velocity-test
+(deftest equations-test
   ;; S0 -> 0m
   ;; S1 -> 150m
   ;; T0 -> 0s
   ;; T1 -> 10s
-  (is (= 15 (velocity 0 150 0 10))))
+  (testing "Average Velocity"
+    (is (= 15 (average-velocity 0 150 0 10))))
 
-(deftest acceleration-test
   ;; V0 -> 5m
   ;; V1 -> 25m
   ;; T0 -> 0s
   ;; T1 -> 4s
+  (testing "Acceleration"
+    (is (= 5 (acceleration 5 25 0 4))))
 
-  (is (= 5 (acceleration 5 25 0 4))))
-
-(deftest position-test
   ;; S0 -> 10m
   ;; v -> 6m/s
   ;; t -> 8s
-  
-  (is (= 58 (position 10 6 8))))
+  (testing "Position"
+    (is (= 58 (position 10 6 8)))))
+
+(deftest validation-erros 
+  (testing "Avarage velocity missing argument."
+    (is (thrown? clojure.lang.ExceptionInfo
+                 (average-velocity 0 10 0 nil))))
+
+  (testing "Acceleration missing argument"
+    (is (thrown? clojure.lang.ExceptionInfo
+                 (acceleration 0 10 0 nil))))
+
+  (testing "Position missing argument"
+     (is (thrown? clojure.lang.ExceptionInfo
+                  (position 10 0 nil)))))
 
 (run-tests)
